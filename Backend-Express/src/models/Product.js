8ffbae048904e3098f928084752099272f16322b
Miguel Angel <lievano.miguel.3534@eam.edu.co>
2026-03-01 12:04:39 -0500

    se requieren todos los atributos de los productos

--- Backend-Express/src/models/Product.js
+++ Backend-Express/src/models/Product.js
@@ -4,8 +4,8 @@ const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    stock: { type: Number, default: 0 },
-    category: { type: String }
+    stock: { type: Number, default: 0, required: true },
+    category: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
